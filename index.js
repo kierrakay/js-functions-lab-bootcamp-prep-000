1778e32557388ebaaeeb2baa1606d66ac59f886e
merry-paradigm-9237 <k.merriman32@hotmail.com>
2019-06-27 00:59:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ function happyHolidays(string) {
 return "Happy holidays!"
 }
 function happyholdaysTo($name) {
-  return "Happy"
+  return "Happy Holidays, you!"
+}
+function happyHolidayTo($holiday, $name) {
+  return "Happy Independence Day, you!"
+}
+function holidayCountdown($holiday, $days) {
+  return "It's 20 days until Mother's Day!"
 }
